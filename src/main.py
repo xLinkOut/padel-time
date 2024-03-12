2f7c7ac67ca95508cc75c4315478ee38b70fb25b
Luca <luca.cirillo5@gmail.com>
2024-03-12 15:14:31 +0100

    add(server): endpoint per l'autenticazione (registrazione, login/out)

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_login import LoginManager
 from models import db, User
 from dotenv import load_dotenv
 from flask import Flask
-from routes import auth_bp
+from routes import auth
 
 load_dotenv()
 
@@ -31,10 +31,10 @@ def load_user(user_id):
 app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASS')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+# Database
 db.init_app(app)
-
 with app.app_context():
     db.create_all()
 
 # Blueprints
-app.register_blueprint(auth_bp, url_prefix="/auth")
+app.register_blueprint(auth, url_prefix="/auth")
--- src/routes.py
+++ src/routes.py
@@ -1,11 +1,57 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from flask import Blueprint
+from flask import Blueprint, request
+from flask_login import current_user, logout_user
 from models import db, User
+from werkzeug.security import check_password_hash, generate_password_hash
 
-auth_bp = Blueprint("auth", __name__)
+auth = Blueprint("auth", __name__)
 
-@auth_bp.post("/signup")
+@auth.post("/signup")
 def signup():
-    pass
+    data = request.get_json()
+
+    if not (email := data.get("email")):
+        return {"success": False, "message": "Email field is required"}, 400
+    
+    if not (password := data.get("password")):
+        return {"success": False, "message": "Password field is required"}, 400
+    
+    if User.query.filter_by(email=email).first():
+        return {"success": False, "message": "A user with that email already exists"}, 409
+    
+    user = User(email=email, password=generate_password_hash(password))
+    db.session.add(user)
+    db.session.commit()
+
+    return {"success": True, "data": {"id": user.id}}, 201
+
+
+@auth.post("/login")
+def login():
+    data = request.get_json()
+
+    if not (email := data.get("email")):
+        return {"success": False, "message": "Email field is required"}, 400
+    
+    if not (password := data.get("password")):
+        return {"success": False, "message": "Password field is required"}, 400
+    
+    user = User.query.filter_by(email=email).first()
+    if not user or not check_password_hash(user.password, password):
+        return {"success": False, "message": "Invalid credentials"}, 401
+    
+    user.last_login_at = db.func.now()
+    db.session.commit()
+
+    return {"success": True, "data": {"id": user.id}}, 200
+
+
+@auth.post("/logout")
+def logout():
+    if not current_user.is_authenticated:
+        return {"success": False, "message": "Not logged in"}, 400
+    
+    logout_user()
+    return {"success": True}
