47a2d21d33c919ffc4935d4b292f159b17c15ddd
Luca <luca.cirillo5@gmail.com>
2024-03-12 15:51:16 +0100

    chg(auth): utilizza JWT invece di session

--- src/routes.py
+++ src/routes.py
@@ -1,12 +1,35 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from flask import Blueprint, request
+from functools import wraps
+from typing import Any, Optional
+from flask import Blueprint, current_app, request
 from flask_login import current_user, logout_user
 from models import db, User
 from werkzeug.security import check_password_hash, generate_password_hash
+import jwt
 
 auth = Blueprint("auth", __name__)
+
+def auth_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if not (token := request.headers.get("Authorization", "").replace("Bearer ", "")):
+            return {"success": False, "message": "Authorization header is required"}, 401
+
+        try:
+            data = jwt.decode(token, current_app.secret_key, algorithms=["HS256"])
+            if not (user := User.query.get(data.get("id"))):
+                return {"success": False, "message": "Invalid authentication token"}, 401
+            
+            current_user.set(user)
+        except Exception as e:
+            return {"success": False, "message": "Invalid authentication token"}, 401
+        
+        return f(user, *args, **kwargs)
+    
+    return decorated
+
 
 @auth.post("/signup")
 def signup():
@@ -42,16 +65,18 @@ def login():
     if not user or not check_password_hash(user.password, password):
         return {"success": False, "message": "Invalid credentials"}, 401
     
+    response = {
+        "id": user.id,
+        "role": user.role
+    }
+
+    token = jwt.encode(
+        response,
+        current_app.secret_key,
+        algorithm="HS256"
+    )
+    
     user.last_login_at = db.func.now()
     db.session.commit()
 
-    return {"success": True, "data": {"id": user.id}}, 200
-
-
-@auth.post("/logout")
-def logout():
-    if not current_user.is_authenticated:
-        return {"success": False, "message": "Not logged in"}, 400
-    
-    logout_user()
-    return {"success": True}
+    return {"success": True, "data": {**response, "token": token}}, 200
