1be9428b3b1de63738781b7aefe1a28d846dc28c
Luca <luca.cirillo5@gmail.com>
2024-03-14 18:17:07 +0100

    add(api): creazione di (una prenotazione per) un game

--- src/routes/api.py
+++ src/routes/api.py
@@ -13,14 +13,28 @@ from models.user import UserRole
 
 api_bp = Blueprint("api", __name__)
 
+@api_bp.get("/games/<int:game_id>")
+@login_required
+def get_game(game_id):
+    filters = [Game.id == game_id]
+    
+    if current_user.role != UserRole.ADMIN.value:
+        filters.append(GameUser.user_id == current_user.id)
+    
+    if not (game := Game.query.join(GameUser).filter(*filters).first()):
+        return {"success": False, "message": "Game not found"}, 404
+
+    return {"success": True, "data": game.to_dict()}, 200
+
 
 @api_bp.get("/games")
 @login_required
-def get_games():
+def search_games():
     filters = []
 
     if slot := request.args.get("slot"):
         filters.append(Game.slot == slot)
+
     if current_user.role != UserRole.ADMIN.value:
         filters.append(GameUser.user_id == current_user.id)
     elif user_id := request.args.get("user_id"):
@@ -30,15 +44,51 @@ def get_games():
     return {"success": True, "data": [game.to_dict() for game in games]}, 200
 
 
-@api_bp.get("/games/<int:game_id>")
+@api_bp.post("/games")
 @login_required
-def get_game(game_id):
-    filters = [Game.id == game_id]
-    if current_user.role != UserRole.ADMIN.value:
-        filters.append(GameUser.user_id == current_user.id)
-    
-    game = Game.query.join(GameUser).filter(*filters).first()
-    if not game:
-        return {"success": False, "message": "Game not found"}, 404
+def create_game():
+    data = request.get_json()
 
-    return {"success": True, "data": game.to_dict()}, 200
+    if not (slots := data.get("slots")):
+        return {"success": False, "message": "<slot> field is required"}, 400
+
+    current_slot_date = datetime.now().replace(minute=59, second=59)
+
+    for slot in slots:
+        try:
+            slot = datetime.strptime(slot, "%Y-%m-%dT%H:%M:%S").replace(minute=0, second=0)
+        except ValueError:
+            return {"success": False, "message": "Invalid date format, must be %Y-%m-%dT%H:%M:%S"}, 400
+
+        if not (9 <= slot.hour <= 23):
+            return {"success": False, "message": "Games can only be made between 9 and 23"}, 400
+
+        if slot < current_slot_date:
+            return {"success": False, "message": "Cannot create a game in the past"}, 400
+
+        game = Game.query.join(GameUser).filter(Game.slot == slot).first()
+        
+        if game:
+            if current_user.id in map(lambda game_user: game_user.user_id, game.players):
+                # Potrebbe non essere bloccante, e continuare ad inserire gli eventuali slot successivi
+                return {"success": False, "message": "You already have a reservation for that slot"}, 409
+
+            if len(game.players) >= current_app.players_for_game:
+                return {"success": False, "message": "There is already a full game scheduled for that slot"}, 409
+        else:
+            game = Game(slot=slot, created_by=current_user.id)
+            db.session.add(game)
+        
+        game_user = GameUser(game=game, user=current_user)
+        db.session.add(game_user)
+
+    db.session.commit()
+
+    # Ci sono abbastanza giocatori per iniziare una partita
+    if len(game.players) == current_app.players_for_game:
+        # Logica per l'invio di email/notifica, maniera asincrona
+        # (Celery+Redis, ad esempio, oppure scheduler in background)
+        pass
+
+    return {"success": True, "data": game.to_dict()}, 201
+
